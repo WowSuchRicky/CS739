a87b12ff7326bf7b48fcfa4b0e8cb1f9e6c217b3
Ricky Mutschlechner <mutschlechne@wisc.edu>
2016-09-25 20:23:44 -0500

    added 'streaming' hopefully

---
+++ thrift/tutorial/gen-cpp/Calculator_client.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/transport/TSocket.h>
+#include <thrift/transport/TTransportUtils.h>
+
+#include "../gen-cpp/Calculator.h"
+
+using namespace std;
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+
+using namespace tutorial;
+using namespace shared;
+
+int main() {
+  boost::shared_ptr<TTransport> socket(new TSocket("localhost", 9090));
+  boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+  boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+  CalculatorClient client(protocol);
+
+  try {
+    transport->open();
+
+
+    /* Begin sum() call on the server */
+    cout << "Summing:  " << endl;
+    const long int nums[15] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    const vector<long int> nums_vec(nums, nums + sizeof(nums) / sizeof(nums[0]) );
+    client.sum(nums_vec);
+    
+
+    transport->close();
+  } catch (TException& tx) {
+    cout << "ERROR: " << tx.what() << endl;
+  }
+}
