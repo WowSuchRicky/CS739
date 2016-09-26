a4649cd83ad1ec9b14364c0dfaf09be7e1051ae1
Stephen M. Sturdevant <sturdevant2@wisc.edu>
2016-09-26 17:43:18 -0500

    added a script to run int marshalling benchmark

---
+++ grpc/int_time/plot.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import matplotlib.pyplot as plt
+
+def mean(numbers):
+    return float(sum(numbers)) / max(len(numbers), 1)
+
+times = []
+for i in range(32):
+    times.append([])
+    with open("int_times/" + str(i) + ".txt") as f:
+        times[i] = [int(t) for t in f.read().strip().split('\n')]
+
+avg_first_times = []
+avg_second_times = []
+for i in times:
+    avg_first_times.append(mean(i[0:len(i):2]))
+    avg_second_times.append(mean(i[1:len(i):2]))
+
+
+print avg_first_times
+print avg_second_times
+
+plt.scatter(range(32), avg_first_times)
+plt.show()
