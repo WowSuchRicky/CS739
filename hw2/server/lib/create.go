b548995bfdc2566e2a7066b0257d646520d3a662
Alex Curtis <acurtiz@gmail.com>
2016-10-19 18:39:32 +0000

    slightly modified create

--- hw2/server/lib/create.go
+++ hw2/server/lib/create.go
@@ -10,9 +10,6 @@ func CreateNFS(in *pb.CreateArgs) (*pb.CreateReturn, error) {
 	// get path of directory
 	dir_path, _ := InumToPath(int(in.Dirfh.Inode))
 
-	// TODO: how should do we use input genum?
-	// dir_genum := in.Dirfh.Genum
-
 	// get full path
 	full_path := dir_path + "/" + in.Name
 
@@ -21,9 +18,13 @@ func CreateNFS(in *pb.CreateArgs) (*pb.CreateReturn, error) {
 	// 2) use open() or creat() straight-up with full path
 	// we'll take the 2nd approach for now
 
-	// file_attr := in.Attr // TODO: how should we use attributes provided?
-	new_fd, err := syscall.Creat(full_path, 0)
-	err = syscall.Close(new_fd) // TODO: should we close this immediately?
+	// TODO: how should we use attributes provided? (maybe don't need to)
+	// I think attributes in this case should just be the mode (i.e. 00666 as we use below?)
+	// file_attr := in.Attr
+
+	new_fd, err := syscall.Creat(full_path, 00666)
+	err = syscall.Close(new_fd)
+	// TODO: should we close this immediately as we're doing above?
 
 	// get inode and genum of new file
 	var new_f_info syscall.Stat_t
@@ -31,7 +32,7 @@ func CreateNFS(in *pb.CreateArgs) (*pb.CreateReturn, error) {
 	new_inode := new_f_info.Ino
 	new_genum := uint64(1) // TODO: get genum
 
-	// TODO: what attributes do we return?
+	// what attributes do we return; probably just use the new_f_info from above to populate it
 
 	return &pb.CreateReturn{Newfh: &pb.FileHandle{Inode: new_inode, Genum: new_genum}, Attr: &pb.Attribute{}}, err
 }
