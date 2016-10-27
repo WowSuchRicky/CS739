ad1d25797a0bf887b37101b99567782f0bc22a9c
sturdevant <sturdevant2@wisc.edu>
2016-10-27 14:08:20 +0000

    all purely local data added

--- hw2/server/test/fsbench/run_close.sh
+++ hw2/server/test/fsbench/run_close.sh
@@ -3,5 +3,5 @@
 mkdir -p $1
 
 for i in `seq 1 1000`; do
-    ./open -i blah -n 2 >> ${1}/open_times.txt
+    ./close -i blah -n 2 >> ${1}/close_times.txt
 done
--- hw2/server/test/fsbench/run_read.sh
+++ hw2/server/test/fsbench/run_read.sh
@@ -5,6 +5,6 @@ mkdir -p $1
 for i in `seq 1 1000`; do
     for j in `seq 0 21`; do
         n=$(echo 2^${j} | bc)
-        ./write -i blah -n 2 -b $n >> ${1}/write_times_b_${j}.txt
+        ./read -i blah -n 2 -b $n >> ${1}/read_times_b_${j}.txt
     done
 done
